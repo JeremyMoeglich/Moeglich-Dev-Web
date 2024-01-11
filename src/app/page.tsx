fa185f64143ba55bc74f798b3e3a061eb487a4e6
jeremy <jeremy@moeglich.dev>
2024-01-11 15:55:32 +0100

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
 import { type NextPage } from "next";
 import { Layout } from "~/code/components/Layout";
 import { TopAnimation } from "~/code/components/top_animation";
+import * as Icons from "./_components/icons";
 
 const Home: NextPage = () => {
     return (
         <Layout>
             <TopAnimation></TopAnimation>
             <div className="mt-48 w-full bg-gradient-to-b from-[#120f22] to-[#04074b]">
-                Test
+                <div>Technologies</div>
+                <div className="flex">
+                    <Icons.NextJsIcon />
+                    <Icons.TailwindIcon />
+                    <Icons.TypescriptIcon />
+                    <Icons.ReactIcon />
+                    <Icons.QwikIcon />
+                </div>
             </div>
         </Layout>
     );
